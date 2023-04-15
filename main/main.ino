52b1f9a10950b87b779d58db0cd4a71a85abc083
Ivan Lesnykh <ivan.lesnykh.official@gmail.com>
2023-04-15 17:31:19 +0300

    Added data sending via bluetooth module.

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,15 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial connection(0, 1);
+
 int hallMatrixWidth = 3;
 int hallMatrixLength = 3;
 
 int hallSignals[3][3];
 
 void setup() {
-  Serial.begin(9600);
+  connection.begin(9600);
+//  Serial.begin(9600);
 }
 
 void loop() {
@@ -15,11 +20,31 @@ void loop() {
       analogPin++;
     }
   }
-  printMatrix();
+
+  connection.println(getSendingString());
+//  Serial.println(getSendingString());
+  delay(1000);
 }
 
 int transformHallSignal(int hallSignal){
   return abs(map(hallSignal, 0, 1023, -511, 511));
+}
+
+String getSendingString(){
+  String output;
+  for(int i = 0; i < hallMatrixWidth; i++){
+    for(int j = 0; j < hallMatrixLength; j++){
+      if(i == 0 && j == 0){
+        output.concat(hallSignals[i][j]);
+      }
+      else{
+        output.concat(" ");
+        output.concat(hallSignals[i][j]);
+      }
+    }
+  }
+  output.concat(";");
+  return output;
 }
 
 void printMatrix(){
